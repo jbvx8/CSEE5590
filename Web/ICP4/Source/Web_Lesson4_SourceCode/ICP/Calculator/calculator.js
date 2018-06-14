7346a1ee670a47f6cbfe7aca3e64a4bfb08be22a
Jackie <jbvx8@mail.umkc.edu>
2018-06-14 16:08:25 -0500

    ICP4 - Add pending description to todo list

--- Web/ICP4/Source/Web_Lesson4_SourceCode/ICP/Calculator/calculator.js
+++ Web/ICP4/Source/Web_Lesson4_SourceCode/ICP/Calculator/calculator.js
@@ -4,20 +4,6 @@ CalculatorApp.controller('CalculatorController', ['$scope', function ($scope) {
     $scope.expression = {number: "", operator: "", string: ""};
     // Write code simple calculator operations
     $scope.evaluate = function(expression){
-        // var a = parseInt(expression.a);
-        // var b = parseInt(expression.b);
-        // if (expression.operator == "+") {
-        //     document.getElementById("output").value = a + b;
-        // } else if (expression.operator == "-") {
-        //     document.getElementById("output").value = a - b;
-        // } else if (expression.operator == "*") {
-        //     document.getElementById("output").value = a * b;
-        // } else {
-        //     document.getElementById("output").value = a / b;
-        // }
-        // expression.a = '';
-        // expression.b = '';
-
         var splitString = $scope.expression.number.split(" ");
         var a= parseInt(splitString[0]);
         var b = parseInt(splitString[1]);
--- Web/ICP4/Source/Web_Lesson4_SourceCode/ICP/Todo/todo.js
+++ Web/ICP4/Source/Web_Lesson4_SourceCode/ICP/Todo/todo.js
@@ -3,12 +3,15 @@ var angularTodo = angular.module('angularTodo', []);
 angularTodo.controller('angularTodoC', ['$scope', function ($scope) {
     // define list of items
     $scope.items = [];
+    $scope.pending = 0;
+    $scope.completed = 0;
 
     // Write code to push new item
 
     $scope.submitNewItem = function (item) {
         $scope.items.push({name: item.name, completed: false});
         item.name = "";
+        $scope.pending++;
 
     };
 
@@ -16,11 +19,23 @@ angularTodo.controller('angularTodoC', ['$scope', function ($scope) {
     $scope.completeItem = function (index) {
         // can flip to uncheck in case it was a mistake
         $scope.items[index].completed = !$scope.items[index].completed;
+        if ($scope.items[index].completed == true) {
+            $scope.completed++;
+            $scope.pending--;
+        } else {
+            $scope.completed--;
+            $scope.pending++;
+        }
     };
 
     // Write code to delete item
 
     $scope.deleteItem = function (index) {
+        if ($scope.items[index].completed == true) {
+            $scope.completed--;
+        } else {
+            $scope.pending--;
+        }
         $scope.items.splice(index, 1);
     };
 }]);
