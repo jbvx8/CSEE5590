00a26c4a8bf49ab85e7b2b44010466eabebbf21b
Jackie <jbvx8@mail.umkc.edu>
2018-06-22 22:47:55 -0500

    Added Lab2 part1

---
+++ Web/Lab2/Source/Lab2/YouTube/script.js
@@ -0,0 +1,38 @@
+
+angular.module('youTubeApp', [])
+    .config(function($sceDelegateProvider) {
+        $sceDelegateProvider.resourceUrlWhitelist([
+            'self',
+            'https://www.youtube.com/**'
+        ])
+    })
+    .controller('AppCtrl', function ($scope, $http) {
+        $scope.videoList = new Array();
+
+        $scope.getResults = function () {
+            var query = document.getElementById("query").value;
+            var key = "AIzaSyC-OZ3iu8r3p6SfBwr3H120QUXxR0MXarQ";
+            var baseUrl = "https://www.googleapis.com/youtube/v3/search"
+            if (query != null && query != "") {
+                var url = baseUrl +
+                    "?key=" + key +
+                    "&part=snippet" +
+                    "&type=video" +
+                    "&videoEmbeddable=true" +
+                    "&q=" + query;
+
+                $http.get(url)
+                    .success(function (response) {
+                        $scope.videoList = response.items;
+                    });
+            } else {
+                alert("Please enter a search query.");
+            }
+        };
+
+        $scope.getVidUrl = function (id) {
+            return "https://www.youtube.com/embed/" + id;
+        }
+
+
+    });
