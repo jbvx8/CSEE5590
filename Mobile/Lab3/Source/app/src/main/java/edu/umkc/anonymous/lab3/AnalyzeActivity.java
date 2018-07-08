69257c31a16daf0ced7c4ea5c304103d2e99ab9c
Jackie <jbvx8@mail.umkc.edu>
2018-07-08 14:49:04 -0500

    Lab3 tasks 2 and 3 working

--- Mobile/Lab3/Source/app/src/main/java/edu/umkc/anonymous/lab3/AnalyzeActivity.java
+++ Mobile/Lab3/Source/app/src/main/java/edu/umkc/anonymous/lab3/AnalyzeActivity.java
@@ -1,30 +1,30 @@
 package edu.umkc.anonymous.lab3;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.AsyncTask;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.system.ErrnoException;
-import android.system.Os;
-import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-import com.google.cloud.vision.v1.AnnotateImageRequest;
-import com.google.cloud.vision.v1.AnnotateImageResponse;
-import com.google.cloud.vision.v1.BatchAnnotateImagesResponse;
-import com.google.cloud.vision.v1.EntityAnnotation;
-import com.google.cloud.vision.v1.Feature;
-import com.google.cloud.vision.v1.Image;
-import com.google.cloud.vision.v1.ImageAnnotatorClient;
-import com.google.protobuf.ByteString;
+import com.google.api.client.extensions.android.json.AndroidJsonFactory;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.services.vision.v1.Vision;
+import com.google.api.services.vision.v1.VisionRequestInitializer;
+import com.google.api.services.vision.v1.model.AnnotateImageRequest;
+import com.google.api.services.vision.v1.model.BatchAnnotateImagesRequest;
+import com.google.api.services.vision.v1.model.BatchAnnotateImagesResponse;
+import com.google.api.services.vision.v1.model.EntityAnnotation;
+import com.google.api.services.vision.v1.model.Feature;
+import com.google.api.services.vision.v1.model.Image;
 
 import java.io.ByteArrayOutputStream;
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.PrintStream;
-import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class AnalyzeActivity extends Activity {
@@ -36,57 +36,100 @@ public class AnalyzeActivity extends Activity {
 
         Intent intent = getIntent();
         final Bitmap image = intent.getParcelableExtra("Image");
-        try {
-            Os.setenv("GOOGLE_APPLICATION_CREDENTIALS", "CSEE5590-Lab3-a950b51a7112.json", true);
-        } catch (ErrnoException e) {
-           System.out.println("Sigh");
-        }
-        String path = System.getenv("GOOGLE_APPLICATION_CREDENTIALS");
-        Thread thread = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    detectLabels(image);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-        });
-        thread.start();
+
+        ImageView userPhoto = (ImageView) findViewById(R.id.img_analyze);
+        userPhoto.setImageBitmap(image);
+        new GetResultsTask().execute(image);
     }
 
-    public static void detectLabels(Bitmap bmp) throws Exception, IOException {
+    public static List<EntityAnnotation> detectLabels(Bitmap bmp) throws Exception, IOException {
         List<AnnotateImageRequest> requests = new ArrayList<>();
 
+        // convert Bitmap to byte[]
         ByteArrayOutputStream stream = new ByteArrayOutputStream();
         bmp.compress(Bitmap.CompressFormat.PNG, 100, stream);
         byte[] imageBytes = stream.toByteArray();
+        stream.close();
 
-        ByteString imgBytes = ByteString.copyFrom(imageBytes);
+        // convert byte array to Google Image object
+        Image inputImage = new Image().encodeContent(imageBytes);
 
+        // create request object (AnnotateImageRequest) with Feature extraction type and image
+        Feature feat = new Feature().setType("LABEL_DETECTION");
+        AnnotateImageRequest request = new AnnotateImageRequest();
+        request.setImage(inputImage).setFeatures(Arrays.asList(feat));
 
+        // create a batch request as expected by api
+        BatchAnnotateImagesRequest batchRequest = new BatchAnnotateImagesRequest();
+        batchRequest.setRequests(Arrays.asList(request));
 
-        Image img = Image.newBuilder().setContent(imgBytes).build();
-        Feature feat = Feature.newBuilder().setType(Feature.Type.LABEL_DETECTION).build();
-        AnnotateImageRequest request =
-                AnnotateImageRequest.newBuilder().addFeatures(feat).setImage(img).build();
-        requests.add(request);
+        // get the google api client
+        Vision vision = getVision();
 
-        try (ImageAnnotatorClient client = ImageAnnotatorClient.create()) {
-            BatchAnnotateImagesResponse response = client.batchAnnotateImages(requests);
-            List<AnnotateImageResponse> responses = response.getResponsesList();
+        // execute request with the client and capture the response
+        BatchAnnotateImagesResponse response =
+                vision.images().annotate(batchRequest).execute();
 
-            for (AnnotateImageResponse res : responses) {
-                if (res.hasError()) {
-                    Log.e("Error: %s\n", res.getError().getMessage());
-                    return;
-                }
+        return response.getResponses().get(0).getLabelAnnotations();
+    }
 
-                // For full list of available annotations, see http://g.co/cloud/vision/docs
-                for (EntityAnnotation annotation : res.getLabelAnnotationsList()) {
-//                    annotation.getAllFields().forEach((k, v) -> out.printf("%s : %s\n", k, v.toString()));
-                }
-            }
+    public static Vision getVision() {
+        Vision.Builder visionBuilder = new Vision.Builder(
+                new NetHttpTransport(),
+                new AndroidJsonFactory(),
+                null
+        );
+        visionBuilder.setVisionRequestInitializer(
+                new VisionRequestInitializer(BuildConfig.GoogleAPIKey));
+
+        return visionBuilder.build();
+    }
+
+    public void printResults(List<EntityAnnotation> labels) {
+        TextView labelText = (TextView) findViewById(R.id.text_label_results);
+        TextView scoreText = (TextView) findViewById(R.id.text_confidence_results);
+        String labelResult = "";
+        String scoreResult = "";
+        for (int i = 0; i < 5; i++) {
+            String description = labels.get(i).getDescription();
+            String score = labels.get(i).getScore().toString();
+            labelResult += description + "\n";
+            scoreResult += score + "\n";
         }
+
+        labelText.setText(labelResult);
+        scoreText.setText(scoreResult);
+    }
+
+    private class GetResultsTask extends AsyncTask<Bitmap, Void, List<EntityAnnotation>> {
+        private ProgressDialog progress = new ProgressDialog(AnalyzeActivity.this);
+
+        @Override
+        protected void onPreExecute() {
+            this.progress.setMessage("Please wait");
+            this.progress.show();
+        }
+        @Override
+        protected List<EntityAnnotation> doInBackground(Bitmap... bmp) {
+            List<EntityAnnotation> labels = new ArrayList<>();
+            try {
+                labels = detectLabels(bmp[0]);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return labels;
+        }
+
+        protected void onPostExecute(List<EntityAnnotation> labels) {
+            if (progress.isShowing()) {
+                progress.dismiss();
+            }
+            printResults(labels);
+        }
+    }
+
+    public void redirectToHomePage(View v) {
+        Intent redirect = new Intent(AnalyzeActivity.this, MainActivity.class);
+        startActivity(redirect);
     }
 }
